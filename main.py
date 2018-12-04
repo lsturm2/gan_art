f4c5a62204b66513099094858dee3c996e2ecc69
Kally Zheng <kallyzheng@DN0a1f4a72.SUNet>
2018-12-03 21:47:46 -0800

    The imports

--- main.py
+++ main.py
@@ -1,3 +1,14 @@
+# Import TensorFlow >= 1.10 and enable eager execution
+import tensorflow as tf
+tf.enable_eager_execution()
+
+import os
+import time
+import numpy as np
+import matplotlib.pyplot as plt
+import PIL
+from IPython.display import clear_output
+
 def main():
 	print("A+")
 
